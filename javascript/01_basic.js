3541311db55e9b3dfd3318276a9199acf64e29d3
SOUMYA GHOSH <153527557+SOUMYAGHOSH123-fresher@users.noreply.github.com>
2024-06-23 18:07:43 +0000

    container config done

---
+++ javascript/01_basic.js
@@ -0,0 +1,50 @@
+"use strict";
+// console.log("hello")
+
+//use datatypes
+const accountId = 423263
+var accountEmail = "soumya@gmail.com" //Donot use 'var' keyword in anywhere
+let accountPassword = "123538"
+let accoutCity = "mayurbhanj"
+/*console.table([accountEmail,accountId,accountPassword,accoutCity])*/
+
+//use datatypes
+let integerValue = 22
+let stringValue = "soumya"
+let booleanValue = false
+let symbolvalue = "@"
+/*console.log(typeof 22)
+console.log(typeof null)
+console.log(typeof undifined)*/
+
+
+//convaersion datatype
+let score = "33abc"
+let valueNumber = Number(score)
+/*console.log(typeof score)
+console.log(typeof valueNumber)
+console.table([valueNumber])*/
+
+
+//---------OPERATION------------
+let str1 = 1
+let str2 = "5"
+let str3 = 2 + 3 + true
+/*console.log(str3)
+console.log(+true)
+console.log(true+)  // throw ERROR
+console.log(+"")*/
+
+
+//------Comparision--------
+/* console.log("2" < 1); //Not recomanded to use this type of comparision
+console.log("2" > 1) 
+console.log(null < 0)
+console.log(null > 0)
+console.log(null >= 0) //the reason that > || >= work differently
+console.log(null == 0)
+console.log("2" === 2)  //Its check both adress and datatype
+console.log("2" == 2)   //Its check only adress value  */
+
+console.log(null > 0)
+
